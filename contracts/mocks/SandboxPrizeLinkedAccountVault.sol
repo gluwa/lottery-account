d9a8dd5bef99482331e4fea0b8b146489cdfe4ec
kevinnguyen17 <79157691+kevinnguyen17@users.noreply.github.com>
2022-04-05 11:01:08 -0700

    SMC-30: Remove unused event

--- contracts/mocks/SandboxPrizeLinkedAccountVault.sol
+++ contracts/mocks/SandboxPrizeLinkedAccountVault.sol
@@ -4,8 +4,6 @@ import "../PrizeLinkedAccountVault.sol";
 
 contract SandboxPrizeLinkedAccountVault is PrizeLinkedAccountVault {
 
-    event DrawResult(uint256 indexed drawTimeStamp, uint256 winningTicket);
-
     function makeDrawV1_Dummy(uint256 drawTimeStamp, uint256 seed)
         external
         onlyOperator
