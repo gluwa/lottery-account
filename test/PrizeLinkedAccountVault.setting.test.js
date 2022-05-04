643511d9f851c9a401bca979cfe49a2147c958bc
ohyung1287 <ohyung1287@gmail.com>
2022-05-04 09:52:50 -0400

    update version in test

--- test/PrizeLinkedAccountVault.setting.test.js
+++ test/PrizeLinkedAccountVault.setting.test.js
@@ -202,7 +202,7 @@ describe('PLSA Vault Setting Checks', function () {
   });
 
   it('check contract version', async function () {
-    expect(await prizeLinkedAccountVault.getVersion()).to.equal("1.9");
+    expect(await prizeLinkedAccountVault.getVersion()).to.equal("2.0");
   });
 
 });
