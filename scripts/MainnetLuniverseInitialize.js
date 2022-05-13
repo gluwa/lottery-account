fff7ed77cedba0cd3d3eb7dde866b276bddb80c9
Jeremy Frank <jeremy.frank@gluwa.com>
2022-05-13 07:21:01 -0600

    add simple multi-day prize test

--- scripts/MainnetLuniverseInitialize.js
+++ scripts/MainnetLuniverseInitialize.js
@@ -3,7 +3,7 @@ const { hexZeroPad } = require('ethers/lib/utils');
 const abi = require('../test/LuniverseTest/abi');
 // const PLSASandBoxAddress = "0xd69dbd8efd2df7a18265e3d708b7042414d30d64";//Staging proxy
 const PLSASandBoxAddress = "0x4988fa8a091ca9ceba24daa0163d326d10b99a80";//PRD 
-const operator = "***REMOVED***";
+const operator = "0xBAFCb499A3147dc72A1572B70aBC59c74fFd7687";
 const privateKey = "";
 // const privateKey = "";//PRD boosting fund address
 const luniversePRC = "http://baas-rpc.luniverse.io:8545?lChainId=3158073271666164067";  
@@ -23,7 +23,7 @@ const ticketPerToken = 1;
 const processingCap = 110;
 const winningChanceFactor = 13;
 
-const boostingProvider="***REMOVED***";
+const boostingProvider="0xbD1eaB0976DF35A5a515B78e2bC6b44458F0C838";
 async function main(){
     var provider = new ethers.providers.JsonRpcProvider(luniversePRC);
     var owner = new ethers.Wallet(privateKey,provider);
